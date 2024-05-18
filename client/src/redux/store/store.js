f43c63c7a2636623e5728aa7b2b69997027e0a14
suraj <alphaguy433@gmail.com>
2024-05-18 15:15:01 +0530

    Added backend files only

--- client/src/redux/store/store.js
+++
@@ -1,9 +0,0 @@
-import {configureStore} from '@reduxjs/toolkit'
-import userProfileSlice from '../features/userProfileSlice.js'
-
-const store=configureStore({
-    reducer:{
-        user:userProfileSlice,
-    }
-})
-export default store
